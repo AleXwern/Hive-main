97859b83b81a5b97da638984ba0099ea52de9c33
AleXwern Nisutoromu <alex.nystrom5@gmail.com>
2020-03-25 00:56:30 +0200

    Cont -> GFX load and show properly

--- wolf3d/src/gfx.c
+++ wolf3d/src/gfx.c
@@ -6,7 +6,7 @@
 /*   By: AleXwern <alex.nystrom5@gmail.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/06 12:41:51 by anystrom          #+#    #+#             */
-/*   Updated: 2020/03/24 01:52:20 by AleXwern         ###   ########.fr       */
+/*   Updated: 2020/03/25 00:53:22 by AleXwern         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,6 +51,10 @@ void	comp_gfx(t_wolf *wolf)
 	wolf->gfx[0] = gfx_get(wolf, ft_strjoin(bpath, "/sky.xpm"), 1080, 360);
 	wolf->gfx[1] = gfx_get(wolf, ft_strjoin(bpath, "/floor.xpm"), 128, 128);
 	wolf->gfx[2] = gfx_get(wolf, ft_strjoin(bpath, "/wall.xpm"), 64, 64);
+	wolf->gfx[3] = gfx_get(wolf, ft_strjoin(bpath, "/stairu.xpm"), 64, 64);
+	wolf->gfx[4] = gfx_get(wolf, ft_strjoin(bpath, "/staird.xpm"), 64, 64);
+	wolf->gfx[5] = gfx_get(wolf, ft_strjoin(bpath, "/door.xpm"), 64, 64);
+	wolf->gfx[6] = gfx_get(wolf, ft_strjoin(bpath, "/warp.xpm"), 64, 64);
 	//mlx_put_image_to_window(wolf->mlx, wolf->win, wolf->gfx[0].img, 0, 0);
 	//mlx_put_image_to_window(wolf->mlx, wolf->win, wolf->gfx[1].img, 0, 360);
 	free(bpath);
